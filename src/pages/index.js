34df6fd202b8fa771b559cdcbcc9262e721ee770
Jonathan Igner <jonathan.igner@gmail.com>
2021-02-17 07:23:17 -0800

    add navbar, thru Lession 12

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <div>
       <h1>Hello folks!</h1>
-      {/* Can only use Link for internal links */}
-      <Link to="/blog/">blog</Link>
+      <Navbar />
+      <div>
+        {/* Can only use Link for internal links */}
+        <Link to="/blog/">blog</Link>
+      </div>
       <a href="https://www.gatsbyjs.org">gatsby docs</a>
     </div>
   )
