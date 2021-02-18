4d4e1fd29a867f26c856dd29af4bdeecd3a71c9c
Jonathan Igner <jonathan.igner@gmail.com>
2021-02-18 12:40:20 -0800

    Add Layout with children

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <h1>Hello folks!</h1>
-      <Navbar />
       <div>
         {/* Can only use Link for internal links */}
         <Link to="/blog/">blog</Link>
       </div>
       <a href="https://www.gatsbyjs.org">gatsby docs</a>
-    </div>
+    </Layout>
   )
 }
